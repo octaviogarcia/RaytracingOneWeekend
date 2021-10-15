c2b9d0a4946ee538eb53ad61d6accdb8d119b4d3
Octavio Garcia Aguirre <ogarcia@frsf.utn.edu.ar>
2021-10-15 13:47:45 -0300

    Mat3x3, decent try at getting paralleograms

--- src/traced.rs
+++ src/traced.rs
@@ -1,7 +1,7 @@
 
 
 use crate::vec3;
-use vec3::{Vec3,UnitVec3,Point3};
+use vec3::{Vec3,UnitVec3,Point3,Mat3x3};
 use crate::utils::{INF};
 use crate::ray::Ray;
 use crate::hits::HitRecord;
@@ -100,136 +100,61 @@ impl Traced for InfinitePlane {
     }
 }
 
+pub type Triangle = Parallelogram;
 #[derive(Copy, Clone)]
 pub struct Parallelogram {//Easier in parametric [f(t,s) = u*t+v*s+origin] form
     pub origin: Point3,
-    pub normal: UnitVec3,
     pub u: UnitVec3,
-    pub v: UnitVec3,
     pub u_length: f32,
+    pub v: UnitVec3,
     pub v_length: f32,
+    pub uxv: UnitVec3,
+    pub ubase_inv: Mat3x3,//Transforms a point (X,Y,Z) to the plane local coordinates (UCOORD,_,_)
+    pub vbase_inv: Mat3x3,//Transforms a point (X,Y,Z) to the plane local coordinates (_,VCOORD,_)
     pub material: Material,
 }
 
 impl Parallelogram {
     pub fn new(origin: &Point3,u: &UnitVec3,v: &UnitVec3,u_length: f32,v_length: f32,material: &Material) -> Self{
-        //Rust doesn't have strict useful type aliases... so we re-unit u and v
-        return Self{origin: *origin,normal: u.cross(*v).unit(),u: u.unit(),v: v.unit(),u_length: u_length,v_length: v_length,material: *material};
+        let u_unit = u.unit();
+        let v_unit = v.unit();
+        let uxv = u_unit.cross(v_unit).unit();
+        let ubase_inv = Mat3x3::new3v(&u_unit,&uxv,&uxv.cross(u_unit).unit()).inverse();
+        let vbase_inv = Mat3x3::new3v(&v_unit,&uxv,&uxv.cross(v_unit).unit()).inverse();
+        return Self{origin: *origin,material: *material,
+            u: u_unit,u_length: u_length,v: v_unit,v_length: v_length,uxv: uxv,
+            ubase_inv: ubase_inv,vbase_inv: vbase_inv};
     }
     pub fn new3points(origin: &Point3,upoint: &Point3,vpoint: &Point3,material: &Material) -> Self{
         let u_rel = *upoint-*origin;
         let u_length = u_rel.length();
+        let u_unit = u_rel/u_length;
+
         let v_rel = *vpoint-*origin;
         let v_length = v_rel.length();
-        return Self{origin: *origin,normal: u_rel.cross(v_rel).unit(),u: u_rel/u_length,v: v_rel/v_length,u_length: u_length,v_length: v_length,material: *material};
+        let v_unit = v_rel/v_length;
+        return Self::new(origin,&u_unit,&v_unit,u_length,v_length,material);
     }
-}
-
-/*
-As the base is not orthonormal (its not a square), we can't just dot product, we need to correct due skewiness introduced by the other vector 
-1) If we did just a dot product with u, in the zone "X" these points would get excluded because (x . u) > u_length
-2) What we do instead is we obtain the vector "->" (named "corrected"), and then project that
-1)      v_____   2)  v_____ 
-        /..X./       /->X./
-       / .../       / .../
-      /  ../       /  ../
-     /   ./       /   ./ 
-    o____ u      o____ u  
-*/
-#[inline]
-fn project_onto_vector(basis: &[&UnitVec3;2],point: &Point3,basis_idx: usize) -> f32{
-    //assert basis_idx < basis.len()
-    let onto  = *basis[basis_idx];
-    let other = *basis[1 - basis_idx];
-    let corrected = *point - point.dot(other)*other;
-    //"onto" is unit length, and they are codirectional with "corrected" so it gets the length with sign
-    //|uvector||u|cos t = |uvector|cos t
-    return onto.dot(corrected);
 }
 
 impl Traced for Parallelogram {
     fn hit(&self,r: &Ray,t_min: f32,t_max: f32) -> Option<HitRecord> {
-        let (root,normal_dot_dir) = ray_plane_intersect(r,&self.normal,&self.origin);
+        let (root,normal_dot_dir) = ray_plane_intersect(r,&self.uxv,&self.origin);
         if root == INF || root < t_min || root > t_max {
             return None;
         }
         let point = r.at(root);
         let point_from_origin = point - self.origin;
-        let ucoord = project_onto_vector(&[&self.u,&self.v],&point_from_origin,0);
-        if ucoord < 0. || ucoord > self.u_length{
+        let ucoord = self.ubase_inv.dot(&point_from_origin).e[0];
+        let vcoord = self.vbase_inv.dot(&point_from_origin).e[0];
+        if ucoord < 0. || vcoord < 0. || ucoord > self.u_length || vcoord > self.v_length {
             return None;
-        }
-        let vcoord = project_onto_vector(&[&self.u,&self.v],&point_from_origin,1);
-        if vcoord < 0. || vcoord > self.v_length{
-            return None;
-        }
-        let outward_normal = normal_against_direction(&self.normal,normal_dot_dir);
+        } 
+        let outward_normal = normal_against_direction(&self.uxv,normal_dot_dir);
         return Some(HitRecord{t: root,point: point,normal: outward_normal,material: self.material});
     }
 }
 
-
-#[derive(Copy, Clone)]
-pub struct Triangle {
-    pub origin: Point3,
-    pub normal: UnitVec3,
-    pub u: UnitVec3,
-    pub v: UnitVec3,
-    pub w: UnitVec3,
-    pub u_length: f32,
-    pub v_length: f32,
-    pub w_length: f32,
-    pub material: Material,
-}
-
-impl Triangle {
-    pub fn new(origin: &Point3,u: &UnitVec3,v: &UnitVec3,u_length: f32,v_length: f32,material: &Material) -> Self{
-        //Rust doesn't have strict useful type aliases... so we re-unit u and v
-        let u_un = u.unit();
-        let v_un = v.unit();
-        let w = (u_un*u_length) - (v_un*v_length);
-        let w_length = w.length();
-        return Self{origin: *origin,normal: u_un.cross(v_un).unit(),u: u_un,v: v_un,w: w/w_length,u_length: u_length,v_length: v_length,w_length: w_length,material: *material};
-    }
-    pub fn new3points(origin: &Point3,upoint: &Point3,vpoint: &Point3,material: &Material) -> Self{//@BUG SOMEWHERE!!!
-        let u_rel = (*upoint)-(*origin);
-        println!("u {}",u_rel);
-        let u_length = u_rel.length();
-        let v_rel = (*vpoint)-(*origin);
-        println!("v {}",v_rel);
-        let v_length = v_rel.length();
-        let w_rel = u_rel - v_rel;
-        let w_length = w_rel.length();
-        return Self{origin: *origin,
-            normal: u_rel.cross(v_rel).unit(),
-            u: (u_rel/u_length),u_length: u_length,
-            v: (v_rel/v_length),v_length: v_length,
-            w: (w_rel/w_length),w_length: w_length,
-            material: *material};
-    }
-}
-
-impl Traced for Triangle {
-    fn hit(&self,r: &Ray,t_min: f32,t_max: f32) -> Option<HitRecord> {
-        let (root,normal_dot_dir) = ray_plane_intersect(r,&self.normal,&self.origin);
-        if root == INF || root < t_min || root > t_max {
-            return None;
-        }
-        let point = r.at(root);
-        let point_from_origin = point - self.origin;
-        //@SPEED: Triangles might have an easier and simpler implementation. I think you can just check with 2 multiplys, Havent though about it too much
-        let ucoord = project_onto_vector(&[&self.u,&self.v],&point_from_origin,0)/self.u_length;
-        if ucoord < 0. || ucoord > 1.{
-            return None;
-        }
-        let vcoord = project_onto_vector(&[&self.u,&self.v],&point_from_origin,1)/self.v_length;
-        if vcoord < 0. || vcoord > 1. || (ucoord+vcoord) > 1.{//Aditional check compared to Parallegram
-            return None;
-        }
-        let outward_normal = normal_against_direction(&self.normal,normal_dot_dir);
-        return Some(HitRecord{t: root,point: point,normal: outward_normal,material: self.material});
-    }
-}
 /*
 #[derive(Copy, Clone)]
 pub struct Cube {
